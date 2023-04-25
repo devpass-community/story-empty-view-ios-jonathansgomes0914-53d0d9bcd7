0327b55a77d89c0385c804f70b829a39f51a3b19
Jonathan Gomes <jonathangomes.ti@gmail.com>
2023-04-25 14:22:28 -0300

    implements label empty view

--- EvercodeApp/Screens/Components/EmptyView.swift
+++ EvercodeApp/Screens/Components/EmptyView.swift
@@ -1,6 +1,42 @@
 import UIKit
 
 class EmptyView: UIView {
+    lazy var label: UILabel = {
+        let view = UILabel(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "No data found"
+        view.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        view.textAlignment = .center
+        view.backgroundColor = .white
+        return view
+    }()
     
-    // TODO
+    init() {
+        super.init(frame: .zero)
+        self.setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension EmptyView {
+
+    func setupViews() {
+        self.backgroundColor = .white
+        self.configureSubviews()
+        self.configureSubviewsConstraints()
+    }
+
+    func configureSubviews() {
+        addSubview(label)
+    }
+
+    func configureSubviewsConstraints() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
 }
